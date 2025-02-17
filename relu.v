80edb354e3b2a0970b0b66f6bc1ed90e122677f5
ArjunG12 <46287498+ArjunG12@users.noreply.github.com>
2025-02-17 14:10:26 +0530

    Create relu.v

---
+++ relu.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.01.2025 21:54:03
+// Design Name: 
+// Module Name: relu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module relu(
+    input [15:0] in_pix,
+    output [15:0] relu_
+);
+    assign relu_ = in_pix[15] ? 15'b0 : in_pix[15:0];
+endmodule
